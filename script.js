d7c4ffca404f61cff05d199a3163cae431f3233d
budaksundaasli2 <budaksundaasli2@gmail.com>
2025-07-01 22:25:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -299,7 +299,7 @@ function generateImage(callback) {
             };
 
             const maxTextWidth = hdSize - (hdSize * 0.167);
-            const wrappedLines = smartWordWrap(text.textContent, maxTextWidth);
+            const wrappedLines = smartWordWrap(text.innerText, maxTextWidth);
             const totalTextHeight = wrappedLines.length * lineHeight;
             const startY = (hdSize / 2) - (totalTextHeight / 2) + (lineHeight / 2);
 
